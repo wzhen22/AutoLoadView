bd6622ad8788f8101fd8e327f449b6639cc434fe
wzhen22 <qwaszx375204@126.com>
2014-03-18 19:39:39 -0700

    Initial Commit

---
+++ AutoLoadView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLoadView
+//
+//  Created by 传晟 on 14-3-18.
+//  Copyright (c) 2014年 传晟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLoadView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLoadView
+//
+//  Created by 传晟 on 14-3-18.
+//  Copyright (c) 2014年 传晟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
